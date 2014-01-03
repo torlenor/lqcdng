07b59f9ade462db51098c41aa32b93feb6f5db64
Hans-Peter Schadler <hps@abyle.org>
2014-01-03 15:16:05 +0100

    Increased the number of equis in runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -17,10 +17,10 @@ else
 	exit
 fi
 echo "Performing test run..."
-echo "Calling './puresu2gauge -s 8 -t 4 -b 5.00 -n 10 -k 5 -e 10 -c -m' ..."
-./puresu3gauge -s 8 -t 4 -b 5.00 -n 10 -k 5 -e 10 -c -m
-echo "Calling './puresu3gauge -s 8 -t 4 -b 5.00 -n 10 -k 5 -e 10 -c -m' ..."
-./puresu2gauge -s 8 -t 4 -b 5.00 -n 10 -k 5 -e 10 -c -m
+echo "Calling './puresu2gauge -s 8 -t 4 -b 5.00 -n 10 -k 5 -e 20 -c -m' ..."
+./puresu3gauge -s 8 -t 4 -b 5.00 -n 10 -k 5 -e 40 -c -m
+echo "Calling './puresu3gauge -s 8 -t 4 -b 5.00 -n 10 -k 5 -e 20 -c -m' ..."
+./puresu2gauge -s 8 -t 4 -b 5.00 -n 10 -k 5 -e 40 -c -m
 echo "Cleaning up..."
 cleanup
 cd ..
